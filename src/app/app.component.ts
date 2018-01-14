0cbf6ea380efb529a97c5fefb102fbf3edbecbf8
Tamás Munkácsi <tomi.munkacsi@gmail.com>
2018-01-14 10:53:33 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = 'HomePage';
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+
+    });
+  }
+}
