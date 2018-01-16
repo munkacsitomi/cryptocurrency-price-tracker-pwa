6f7b628f441251fd0cdcdcbd603f76b18387d390
Tamás Munkácsi <tomi.munkacsi@gmail.com>
2018-01-16 19:53:07 +0100

    add percentage increase calculator pipe

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
+import { PercentageIncreaseCalcPipe } from '../../pipes/percentageIncreaseCalc.pipe';
 
 @NgModule({
   declarations: [
     HomePage,
+    PercentageIncreaseCalcPipe
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
